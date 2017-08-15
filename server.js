2eef5a91e6978d1c32f56faece92c9518f980fcf
Ruchi-Sharma-06 <30696980+Ruchi-Sharma-06@users.noreply.github.com>
2017-08-15 22:32:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function(req,res){
+
+app.get('/article-one', function(req,res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+
 app.get('/article-two', function(req,res){
    res.send('Article two requested and will be the server here'); 
 });
+
 app.get('/article-three', function(req,res){
    res.send('Article three requested and will be the server here'); 
 });
